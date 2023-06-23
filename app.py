398c165d5ede7e33bae27dc7ff37aced1b57881a
Valentina Pasqual <valentina.pasqual@studio.unibo.it>
2023-06-23 15:28:14 +0200

    fix + contacts

--- app.py
+++ app.py
@@ -6,10 +6,6 @@ import uuid
 import requests
 import json
 
-#from views import views
-#from routes import routes
-
-
 app = Flask(__name__, static_folder="assets")
 
 # endpoint
@@ -120,8 +116,6 @@ def home():
     return render_template('index.html',  cardsResults = cardsResults, storiesResults = storiesResults, suitList = suitList, typologyList = typologyList, meaningsResults = meaningsResults, classList = classList, networkResults = networkResults)
 
 # SET THE ENDPOINT
-
-
 
 # KG BROWSING
 @app.route('/storie/<storyID>')
@@ -447,82 +441,15 @@ def meaning(meaningID):
 def send_static(path):
     return send_from_directory('static', path)
 
+@app.route('/contatti')
+def contacts():
+    return render_template('contacts.html')
+
 # ENDPOINT
 
 @app.route("/sparql", methods=['GET', 'POST'])
 def sparql_gui(active=None):
-	""" SPARQL endpoint GUI and request handler
-
-	Parameters
-	----------
-	active: str
-		Query string or None
-		If None, renders the GUI, else parse the query (__run_query_string)
-		If the query string includes an update, return error, else sends
-		the query to the endpoint (__contact_tp)
-	"""
-	if request.method == 'GET':
-		content_type = request.content_type
-		q = request.args.get("query")
-		return __run_query_string(active, q, content_type)
-	else:
-		content_type = request.content_type
-		cur_data = request.get_data()
-		if "application/x-www-form-urlencoded" in content_type:
-			return __run_query_string(active, cur_data, True, content_type)
-		elif "application/sparql-query" in content_type:
-			return __contact_tp(cur_data, True, content_type)
-		else:
-			return render_template('sparql.html',active=active)
-
-def __run_query_string(active, query_string,
-	is_post=False, content_type="application/x-www-form-urlencoded"):
-	try:
-		query_str_decoded = query_string.decode('utf-8')
-	except Exception as e:
-		query_str_decoded = query_string
-	parsed_query = parse_qs(query_str_decoded)
-
-	if query_str_decoded is None or query_str_decoded.strip() == "":
-		return render_template('sparql.html',active=active)
-
-	if re.search("updates?", query_str_decoded, re.IGNORECASE) is None:
-		if "query" in parsed_query or "select" in query_str_decoded.lower():
-			return __contact_tp(query_string, is_post, content_type)
-		else:
-			return render_template('sparql.html',active=active)
-	else:
-		return render_template('403.html'), 403
-
-def __contact_tp(data, is_post, content_type):
-	accept = request.args.get('HTTP_ACCEPT')
-	if accept is None or accept == "*/*" or accept == "":
-		accept = "application/sparql-results+json"
-
-	data = data if isinstance(data,bytes) else quote(data)
-	if is_post:
-		req = requests.post(endpoint, data=data,
-							headers={'content-type': content_type, "accept": accept})
-	else:
-		req = requests.get("%s?query=%s" % (endpoint,data ),
-						   headers={'content-type': content_type, "accept": accept})
-
-	if req.status_code == 200:
-		response = Response()
-		response.headers['Access-Control-Allow-Origin'] = '*'
-		response.headers['Access-Control-Allow-Credentials'] = 'true'
-		response.headers['Content-Type'] = req.headers["content-type"]
-		response.mimetype = "application/sparql-results+json"
-		return req.json()
-	else:
-		return render_template('error.html',
-			status_code=str(req.status_code),
-			headers={"Content-Type": request.content_type},
-			text=req.text)
-
-@app.route('/error')
-def error(status_code, headers, text):
-	return render_template('error.html',status_code, headers, text)
+	return render_template('sparql.html',active=active)
 
 @app.errorhandler(403)
 def page_not_found(e):
