a1948ba5d7bf3a6e90b1e743bbd4d8e9dde70cb0
Valentina Pasqual <45206913+ValentinaPasqual@users.noreply.github.com>
2023-09-29 12:08:09 +0200

    Create stats.js

---
+++ assets/js/stats.js
@@ -0,0 +1,12 @@
+var owa_baseUrl = 'https://www2.classics.unibo.it/stats/';
+var owa_cmds = owa_cmds || [];
+owa_cmds.push(['setSiteId', 'd9057f5f7d9027492898cc1c210f93d7']);
+owa_cmds.push(['trackPageView']);
+owa_cmds.push(['trackClicks']);
+
+(function() {
+    var _owa = document.createElement('script'); _owa.type = 'text/javascript'; _owa.async = true;
+    owa_baseUrl = ('https:' == document.location.protocol ? window.owa_baseSecUrl || owa_baseUrl.replace(/http:/, 'https:') : owa_baseUrl );
+    _owa.src = owa_baseUrl + 'modules/base/dist/owa.tracker.js';
+    var _owa_s = document.getElementsByTagName('script')[0]; _owa_s.parentNode.insertBefore(_owa, _owa_s);
+}());
